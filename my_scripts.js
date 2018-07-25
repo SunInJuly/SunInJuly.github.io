05c9cbda443c4a36ae98e3bba1323178de99e45a
SunInJuly <sun_in_july@mail.ru>
2018-07-25 15:38:15 +0300

    erroe catching added

--- my_scripts.js
+++ my_scripts.js
@@ -15,17 +15,33 @@ function GetURLParameter(sParam)
 
 function httpGet(theUrl)
 {
+    var res;
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
     xmlHttp.send( null );
     return xmlHttp.responseText;
+
+	
+   
 }
 
 
 function GetResult()
-{		
-	var data = JSON.parse(httpGet('http://worldclockapi.com/api/json/utc/now'));
-	var now = new Date(data.currentDateTime);
+{
+	
+try {
+		var raw = httpGet('http://worldclockapi.com/api/json/utc/now');
+		var data = JSON.parse(raw);
+		var now = new Date(data.currentDateTime);
+		console.log ("Using server time: ", now.getUTCHours())
+
+	}
+	catch (e)
+{	
+		var now = new Date();
+		console.log ("Using local time: ", now.getUTCHours())
+	}
 	return Math.log(now.getUTCHours());
+
 	
 }
