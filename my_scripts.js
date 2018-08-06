cc7e9bed03da970ec417133c2a4a4e9024ce251a
alekspog <aleksey.pogibelev@gmail.com>
2018-08-06 13:40:06 +0300

    Update my_scripts.js

--- my_scripts.js
+++ my_scripts.js
@@ -87,9 +87,7 @@ function checkResult(input_value) {
         }
         else {
             alert("Неверный ответ!");
-	    console.log(Math.log(Math.sin(12 * input_value)));
-
-        }
+	}
     }
     else {
         alert("Роботы должны рулить!");
